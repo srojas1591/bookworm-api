775111fef98399340df90dc530cc139a11154cdf
Steven Rojas Chevez <steven.rojas@ibm.com>
2018-08-27 15:15:48 -0600

    Installation BodyParser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import path from "path";
+import mongoose from 'mongoose';
+import auth from "./routes/auth";
+import bodyParser from 'body-parser';
+
 
 const app = express ();
+mongoose.connect('mongodb://localhost/bookworm', {useMongoClient: true});
 
-app.post("/api/auth", (req, res) => {
-    res.status(400).json({errors: {global: "Invalid Credentials"}});
-});
+app.use("/api/auth", auth);
+
+// app.post("/api/auth", (req, res) => {
+//     res.status(400).json({errors: {global: "Invalid Credentials"}});
+// });
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
